008d8d262e50e163d059601b38476242860493cb
cyk90290 <57485322+cyk90290@users.noreply.github.com>
2019-11-08 00:40:19 +0800

    Update and rename homework11.java to superhomework11.java

--- lib/src/main/java/com/example/lib/superhomework11.java
+++ lib/src/main/java/com/example/lib/superhomework11.java
@@ -1,8 +1,6 @@
 package com.example.lib;
-
 import java.util.Scanner;
-
-public class homework11 {
+public class superhomework11 {
         public static void main(String[] s) {
             int x;
             int y ;
@@ -10,28 +8,24 @@ public class homework11 {
             int t ;
             Scanner scanner = new Scanner(System.in);
             System.out.println("Please input number:");
-
             x = scanner.nextInt();
             y = scanner.nextInt();
             z = scanner.nextInt();
-
             if (x > y) {
                 t = x;
                 x = y;
                 y = t;
             }
-
             if (x > z) {
                 t = x;
                 x = z;
                 z = t;
             }
-
             if (y>z){
                 t = y;
                 y = z;
                 z = t;
             }
-            System.out.println(x + "," + y + "," + z);
+            System.out.println(x + " , " + y + " , " + z);
         }
     }
