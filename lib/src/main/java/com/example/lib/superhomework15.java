dc55510174f18fd3596f6990bc882b6657514579
cyk90290 <57485322+cyk90290@users.noreply.github.com>
2019-11-08 00:48:00 +0800

    Update and rename homework15.java to superhomework15.java

--- lib/src/main/java/com/example/lib/superhomework15.java
+++ lib/src/main/java/com/example/lib/superhomework15.java
@@ -1,12 +1,11 @@
 package com.example.lib;
 
-public class homework15 {
+public class superhomework15 {
     public static void main(String[] argv) {
         java.util.Scanner scanner = new
                 java.util.Scanner(System.in);
         int h = scanner.nextInt();
         int m = scanner.nextInt();
-
         {
             if ((h == 7 && m >= 30) || (h <= 17)) {
                 System.out.println("in School");
