0607f2472d145580cb597671e085aa74cbc2d07f
cyk90290 <57485322+cyk90290@users.noreply.github.com>
2019-11-08 01:08:57 +0800

    Update and rename homework5.java to superhomework5.java

--- lib/src/main/java/com/example/lib/superhomework5.java
+++ lib/src/main/java/com/example/lib/superhomework5.java
@@ -1,32 +1,24 @@
 package com.example.lib;
 
-public class homework5 {
+public class superhomework5 {
     public  static  void main(String[] avrg) {
         int hu1,min1,sec1,hu2,min2,sec2;
         java.util.Scanner scanner= new
                 java.util.Scanner(System.in);
-
         System.out.println("input Hour1 :");
          hu1=scanner.nextInt();
-
         System.out.println("input Minute1 :");
          min1=scanner.nextInt();
-
         System.out.println("input Second1 :");
          sec1=scanner.nextInt();
-
         System.out.println("input Hour2 :");
          hu2=scanner.nextInt();
-
         System.out.println("input Minute2 :");
          min2=scanner.nextInt();
-
         System.out.println("input Second2 :");
          sec2=scanner.nextInt();
-
         int allsec1=(hu1*3600)+(min1*60)+sec1;
         int allsec2=(hu2*3600)+(min2*60)+sec2;
-
         System.out.println("Ans:"+Math.abs(allsec1-allsec2));
     }
 }
