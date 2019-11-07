aaf321fe08b698675f970ed68b3bf30fe97ac50a
cryingpotato <qq1591680@gmail.com>
2019-11-08 00:04:41 +0800

    up

---
+++ lib/src/main/java/com/example/lib/homework15.java
@@ -0,0 +1,18 @@
+package com.example.lib;
+
+public class homework15 {
+    public static void main(String[] argv) {
+        java.util.Scanner scanner = new
+                java.util.Scanner(System.in);
+        int h = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        {
+            if ((h == 7 && m >= 30) || (h <= 17)) {
+                System.out.println("in School");
+            } else {
+                System.out.println("out School");
+            }
+        }
+    }
+}
