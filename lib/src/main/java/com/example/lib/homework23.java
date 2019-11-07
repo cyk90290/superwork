8535ae432a1057231b36e933ee4781ded71ecc40
cryingpotato <qq1591680@gmail.com>
2019-11-08 00:08:07 +0800

    up

---
+++ lib/src/main/java/com/example/lib/homework23.java
@@ -0,0 +1,29 @@
+package com.example.lib;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class homework23 {
+    public static void main(String[] args) {
+
+    FileReader fr = null;
+    try {
+        fr = new FileReader("D:\\3A513025.txt");
+        char[] buf = new char[1024];
+        int len = 0;
+        while ((len = fr.read(buf)) != -1) { //讀完時傳回-1
+            System.out.println(new String(buf, 0, len));
+        }
+    } catch (IOException e) {
+        System.out.println("read-Exception :" + e.toString());
+    } finally {
+        if (fr != null) {
+            try {
+                fr.close();
+            } catch (IOException e) {
+                System.out.println("close-Exception :" + e.toString());
+            }
+        }
+    }
+}
+}
