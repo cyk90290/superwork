0c669e291910adf0ceb0e6f627ed43736c75db61
cyk90290 <57485322+cyk90290@users.noreply.github.com>
2019-11-08 01:04:41 +0800

    Update and rename homework3.java to superhomework3.java

--- lib/src/main/java/com/example/lib/superhomework3.java
+++ lib/src/main/java/com/example/lib/superhomework3.java
@@ -1,19 +1,15 @@
 package com.example.lib;
 
-public class homework3 {
+public class superhomework3 {
     public  static  void main(String[] arvg) {
         java.util.Scanner scanner= new
                 java.util.Scanner(System.in);
-
         System.out.println("Weight (kg):");
         double inp1=scanner.nextInt();
-
         System.out.println("Height (cm):");
         double inp2=scanner.nextInt();
-
         double Hei=inp2/100;
         double BMI=inp1/(Hei*Hei);
-
         System.out.println("Your B M I : "+BMI);
     }
 }
