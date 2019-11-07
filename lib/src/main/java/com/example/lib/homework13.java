ab1f4097f3c2d555eba6c14d3789c6264e4ba5ab
cryingpotato <qq1591680@gmail.com>
2019-11-08 00:02:35 +0800

    up

---
+++ lib/src/main/java/com/example/lib/homework13.java
@@ -0,0 +1,22 @@
+package com.example.lib;
+
+import java.util.Scanner;
+
+public class homework13 {
+    public static void main(String[] argv) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(":");
+        char c = input.next().charAt(0);
+        change(c);
+    }
+    private static void change(char c) {
+        if(c>='A' && c<='Z'){
+            c+=32;
+            System.out.println((c));
+        }else if(c>='a' && c<='z'){
+            c-=32;
+            System.out.println((c));
+        }
+    }
+}
+
