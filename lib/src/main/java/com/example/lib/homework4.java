59138201daf823e4cf9cec69c3c1598268633936
cryingpotato <qq1591680@gmail.com>
2019-11-07 22:25:38 +0800

    up

---
+++ lib/src/main/java/com/example/lib/homework4.java
@@ -0,0 +1,15 @@
+package com.example.lib;
+
+public class homework4 {
+    public  static  void main(String[] avrg) {
+        java.util.Scanner scanner= new
+                java.util.Scanner(System.in);
+
+        System.out.println("input second (sec) :");
+        int inp=scanner.nextInt();
+        int hu=inp/3600;
+        int Min=(inp%3600)/60;
+        int sec=(inp%3600)%60;
+        System.out.println("Hour:"+hu+"  Minute:"+Min+"  Second:"+sec);
+    }
+}
